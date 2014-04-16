bc7b5b819831bf95978821a779559d21b646ea73
Michael Orr <michael@cloudspace.com>
2014-04-16 21:00:52 +0000

    initial project generation using the Cloudspace Launcher

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Justintest4::Application.config.secret_key_base = 'f81fc8133a72cf9af889b726058fa1a623ac313a4d2be3e0d2525de8d6892c6817869dee089dc262b9402c64109b22540ec7587231051a8a5bf402d7919f3fd1'
